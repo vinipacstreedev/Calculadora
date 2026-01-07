6297caa418ef9ffd054258e863b3e1ecd9a0fac4
Vinicius Pereira <viniciuss.ssouzaa@gmail.com>
2026-01-07 18:43:59 -0300

    Atualizacoes do commit funcao eval

--- calculo.js
+++ calculo.js
@@ -1,52 +1,17 @@
-const leitura = document.querySelectorAll('.leitura');
-const botaoNumero = document.querySelectorAll('.numero');
-const botaoIgual = document.querySelectorAll('.igual');
-const botaoDecimal = document.querySelectorAll('.decimal');
-const botaoLimpar = document.querySelectorAll('.limpar');
-const botaoOperador = document.querySelectorAll('.operador');
 
+function adicionarCaracter(caracter){
+    const valorDisplay = document.querySelector('.display').value
+    
+    document.querySelector('.display').value = valorDisplay + caracter
+    
+}
 
-let displayValor = '0';
-let primeiroValor = null;
-let operacaoAtual = null;
-let aguardandoSegundoValor = false;
+function limparNumeros(){
+    document.querySelector('.display').value =''
+}
+    
+function calcular(){
+    const valorDisplay = document.querySelector('.display').value
 
-
-
-botaoNumero.forEach(botao => {
-    botao.addEventListener('click', () => {
-        console.log('Número clicado:', botao.textContent);
-    });
-});
-
-botaoOperador.forEach(botao => {
-    botao.addEventListener('click', () => {
-        console.log('Operador clicado:', botao.textContent);
-    });
-});
-
-botaoIgual.forEach(botao => {
-    botao.addEventListener('click', () => {
-        console.log('Igual cliacado', botao.textContent);
-    });
-});
-
-botaoLimpar.forEach(botao => {
-    botao.addEventListener('click', () => {
-        console.log('Limpar cliacado', botao.textContent);
-    });
-});
-
-botaoDecimal.forEach(botao => {
-    botao.addEventListener('click', () => {
-        console.log('Decimal cliacado');
-    });
-});
-
-console.log('displayValor:', displayValor);
-console.log('primeiroValor:', primeiroValor); 
-console.log('operador:', operacaoAtual); 
-console.log('aguardandoSegundoValor:', aguardandoSegundoValor); 
-
-
-
+    document.querySelector('.display').value = eval(valorDisplay)
+}
